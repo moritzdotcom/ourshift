755875665c1c2024b0ad45b2065e454e794f65d9
Moritz Löchner <momo.loechner@gmail.com>
2025-10-12 18:20:26 +0200

    implement open holiday API

---
+++ lib/openHolidays.ts
@@ -0,0 +1,39 @@
+import { dateToISO } from './dates';
+
+type OpenHolidayResponse = {
+  id: string;
+  startDate: string; // "2025-01-01"
+  endDate: string; // "2025-01-01"
+  type: string; // "Public"
+  name: { language: string; text: string }[];
+  regionalScope: string; // "Regional"
+  temporalScope: string; // "FullDay"
+  nationwide: boolean;
+};
+
+export async function fetchPublicHolidays(options: {
+  from?: Date | string;
+  to?: Date | string;
+}) {
+  const from = options?.from ? new Date(options.from) : new Date();
+  const to = options?.to
+    ? new Date(options.to)
+    : new Date(from.getFullYear(), 11, 31);
+  const fromParam = dateToISO(typeof from === 'string' ? new Date(from) : from);
+  const toParam = dateToISO(typeof to === 'string' ? new Date(to) : to);
+
+  const data = await fetch(
+    `https://openholidaysapi.org/PublicHolidays?countryIsoCode=DE&validFrom=${fromParam}&validTo=${toParam}&languageIsoCode=DE&subdivisionCode=DE-NRW`
+  );
+  if (!data.ok) {
+    throw new Error('Failed to fetch public holidays');
+  }
+  const rawData: OpenHolidayResponse[] = await data.json();
+
+  return rawData.map((h) => ({
+    date: h.startDate,
+    name:
+      h.name.find((n) => n.language.toLowerCase() === 'de')?.text ||
+      h.name[0].text,
+  }));
+}
