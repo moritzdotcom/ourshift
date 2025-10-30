faee3e1c88337c34cb13c98ff6dd38ef7fb823fc
Moritz Löchner <momo.loechner@gmail.com>
2025-10-30 10:08:58 +0100

    Danger API route

---
+++ pages/api/danger.ts
@@ -0,0 +1,26 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '@/lib/prismadb';
+
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const shifts = await prisma.shift.findMany({
+    where: { end: { lte: new Date() }, clockIn: null, clockOut: null },
+  });
+
+  const updated = await prisma.$transaction(
+    shifts.map((s) =>
+      prisma.shift.update({
+        where: { id: s.id },
+        data: {
+          clockIn: s.start,
+          clockInSource: 'MANUAL',
+          clockOut: s.end,
+          clockOutSource: 'MANUAL',
+        },
+      })
+    )
+  );
+  return res.status(200).json(updated);
+}
