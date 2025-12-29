a4795cb2dfe74166fb780fc889b17d6630d45327
Moritz Löchner <momo.loechner@gmail.com>
2025-12-29 12:10:47 +0100

    Only active users in timeAccounts dropdown

--- pages/management/timeAccount/[userId].tsx
+++ pages/management/timeAccount/[userId].tsx
@@ -58,7 +58,7 @@ export default function TimeAccountUserPage() {
       const { data } = await axios.get<ApiGetSimpleUsersResponse>(
         '/api/users?simple=true'
       );
-      setUsers(data);
+      setUsers(data.filter((u) => u.isActive));
     }
     fetchUsers();
   }, []);
